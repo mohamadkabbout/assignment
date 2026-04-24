3b51c6fbfde017d0dc55b3e6bdf7b7af6a13ae82
kabbout <mouhamadkabbout4@gmail.com>
2026-04-24 16:07:57 +0300

    Add navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,55 @@
+import './navbar.scss'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="#">
+          My React App
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mainNavbar"
+          aria-controls="mainNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="mainNavbar">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Home
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#services">
+                Services
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
